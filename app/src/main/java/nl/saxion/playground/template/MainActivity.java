34d1d2aa7405592219680650b3f14a96389bc1ae
Michael Cornelisse <459466@student.saxion.nl>
2019-06-14 14:39:52 +0200

    Cleaned MainActivity and manifest removed sample templates from the project

--- app/src/main/java/nl/saxion/playground/template/MainActivity.java
+++ app/src/main/java/nl/saxion/playground/template/MainActivity.java
@@ -3,35 +3,20 @@ package nl.saxion.playground.template;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-
 import nl.saxion.playground.template.lib.GameView;
 import nl.saxion.playground.template.lib.GameModel;
-import nl.saxion.playground.template.spaceshooter.Game;
-import nl.saxion.playground.template.spaceshooter.Activity;
+
+
 // Teacher: This class has to be removed from master branch.
 public class MainActivity extends AppCompatActivity {
 
-    GameView spaceShooterCanvas, platformerCanvas,lumberjackCanvas;
-    GameModel spaceShooter, platformer, lumberjack;
+    GameView lumberjackCanvas;
+    GameModel lumberjack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        spaceShooterCanvas = findViewById(R.id.spaceShooter);
-        spaceShooter = new Game();
-
-
-
-        findViewById(R.id.spaceShooterText).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Activity.class));
-            }
-        });
 
         lumberjackCanvas = findViewById(R.id.platformer);
         lumberjack = new nl.saxion.playground.template.lumberjack_simulator.Game();
@@ -39,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
 
         startActivity(new Intent(MainActivity.this, nl.saxion.playground.template.lumberjack_simulator.Activity.class));
 
-        platformer = new nl.saxion.playground.template.platformer.Game();
 
 //        TextView lumberjackTV = findViewById(R.id.platformerText);
 //        lumberjackTV.setText("LUMBERJACK !!!");
@@ -54,14 +38,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        spaceShooterCanvas.setGame(spaceShooter);
         lumberjackCanvas.setGame(lumberjack);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        spaceShooterCanvas.setGame(null);
         lumberjackCanvas.setGame(null);
     }
 }
