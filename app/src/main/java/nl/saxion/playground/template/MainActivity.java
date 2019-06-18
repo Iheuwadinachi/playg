8af44cd40c6139645442e5c63061e252246e485c
iheuwadinachi <469962@student.saxion.nl>
2019-06-18 09:54:16 +0200

    settingsMenu

--- app/src/main/java/nl/saxion/playground/template/MainActivity.java
+++ app/src/main/java/nl/saxion/playground/template/MainActivity.java
@@ -4,18 +4,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import nl.saxion.playground.template.lib.GameView;
 import nl.saxion.playground.template.lib.GameModel;
 import nl.saxion.playground.template.spaceshooter.Game;
 import nl.saxion.playground.template.spaceshooter.Activity;
+import nl.saxion.playground.template.spaceshooter.SettingsMenu;
 
 public class MainActivity extends AppCompatActivity {
 
     GameView spaceShooterCanvas, platformerCanvas,lumberjackCanvas;
     GameModel spaceShooter, platformer, lumberjack;
-
+    Button settingBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +26,15 @@ public class MainActivity extends AppCompatActivity {
         spaceShooterCanvas = findViewById(R.id.spaceShooter);
         spaceShooter = new Game();
 
+        settingBtn= findViewById(R.id.Settingbutton);
+        settingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent;
+                intent = new Intent(MainActivity.this,SettingActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
         findViewById(R.id.spaceShooterText).setOnClickListener(new View.OnClickListener() {
@@ -64,4 +75,5 @@ public class MainActivity extends AppCompatActivity {
         spaceShooterCanvas.setGame(null);
         lumberjackCanvas.setGame(null);
     }
+
 }
