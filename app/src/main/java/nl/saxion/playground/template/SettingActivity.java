15d6e50ca73accbe91b11675033a507fa3966b88
iheuwadinachi <469962@student.saxion.nl>
2019-06-21 22:48:37 +0200

    settingsMenu with some check functions

--- app/src/main/java/nl/saxion/playground/template/SettingActivity.java
+++ app/src/main/java/nl/saxion/playground/template/SettingActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,7 +17,9 @@ import nl.saxion.playground.template.platformer.Activity;
 public class SettingActivity extends AppCompatActivity {
     private TextView settingsTextView, soundTextView, musicTextView;
 
-    private Switch soundSwitch,musicSwitch, themeSwitch;
+    private Switch soundSwitch;
+    private Switch musicSwitch;
+    private Switch themeSwitch;
     private Button btnQuitGame;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +27,32 @@ public class SettingActivity extends AppCompatActivity {
 
         setContentView(R.layout.settings);
 
-
         soundSwitch = findViewById(R.id.soundSwitch);
-
+        Switch toggle = (Switch) findViewById(R.id.soundSwitch);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Toast.makeText(getApplicationContext(), "Sound Switch on!", Toast.LENGTH_LONG).show();
+                    } else {
+                    Toast.makeText(getApplicationContext(), "Sound Switch off!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
         musicSwitch = findViewById(R.id.musicSwitch);
+        toggle = (Switch) findViewById(R.id.musicSwitch);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Toast.makeText(getApplicationContext(), "Music Switch on!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Music Switch off!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+
 
         themeSwitch = findViewById(R.id.themeSwitch);
     }
@@ -41,36 +65,55 @@ public class SettingActivity extends AppCompatActivity {
 
 
 
+                btnQuitGame = (Button) findViewById(R.id.quitButton);
+                btnQuitGame.setOnClickListener(new View.OnClickListener() {
 
-        btnQuitGame = (Button) findViewById(R.id.quitButton);
-            btnQuitGame.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        AlertDialog.Builder altquit = new AlertDialog.Builder(SettingActivity.this);
+                        altquit.setMessage("Do you want to Quit this Game").setCancelable(false)
+                                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        finish();
+                                    }
+                                })
+                                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.cancel();
+                                    }
 
-                @Override
-                public void onClick(View v) {
-                    AlertDialog.Builder altquit = new AlertDialog.Builder(SettingActivity.this);
-                    altquit.setMessage("Do you want to Quit this Game").setCancelable(false)
-                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    finish();
-                                }
-                            })
-                            .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.cancel();
-                                }
+                                });
 
-                            });
+                        AlertDialog alert = altquit.create();
+                        alert.setTitle("");
+                        alert.show();
+                    }
+                });
+            }
 
-                    AlertDialog alert = altquit.create();
-                    alert.setTitle("" );
-                    alert.show();
-                }
-            });
-        }
-
-
+    public Switch getSoundSwitch() {
+        return soundSwitch;
     }
 
+    public void setSoundSwitch(Switch soundSwitch) {
+        this.soundSwitch = soundSwitch;
+    }
 
+    public Switch getMusicSwitch() {
+        return musicSwitch;
+    }
+
+    public void setMusicSwitch(Switch musicSwitch) {
+        this.musicSwitch = musicSwitch;
+    }
+
+    public Switch getThemeSwitch() {
+        return themeSwitch;
+    }
+
+    public void setThemeSwitch(Switch themeSwitch) {
+        this.themeSwitch = themeSwitch;
+    }
+}
